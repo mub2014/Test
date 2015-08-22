a19bc28d3be02bc721875b35bb54799d64f41233
renen1996 <renen1996@walla.com>
2015-08-22 16:20:55 +0300

    $git add

---
+++ vbconsole/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim numbers As New List(Of Integer)
+        Dim sum As Integer
+        For i As Integer = 1 To 1000000
+            numbers.Add(i)
+        Next
+        For Each number As Integer In numbers
+            For Each digit As Char In number.ToString
+                sum = sum + Val(digit)
+            Next
+        Next
+        Console.WriteLine(sum)
+        Console.ReadLine()
+    End Sub
+
+End Module
